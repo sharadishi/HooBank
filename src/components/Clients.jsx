f21a0989bc7bc223493221652e28f78c9271dfe4
sharadishi <sharad09ishi@gmail.com>
2022-08-18 16:59:41 +0530

    design changes

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,8 +5,8 @@ import styles from '../style'
 const Clients = () => (
     <section className={`${styles.flexCenter} my-4`}>
         <div className={`${styles.flexCenter} flex-wrap w-full`}>
-            {clients.map((client) => (
-                <div key={clients.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+            {clients.map((client, index) => (
+                <div key={index} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
                     <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain" />
 
                 </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,25 +2,35 @@ import React from 'react'
 import styles from '../style'
 import { logo } from '../assets'
 import { footerLinks, socialMedia } from '../constants'
+
+
 const Footer = () => (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
         <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
             <div className="flex flex-1 flex-col justify-start mr-10">
-                <img src={logo} alt="logo" className="w-[266px] h-[72px] object-contain" />
+                <img
+                    src={logo}
+                    alt="logo"
+                    className="w-[266px] h-[72px] object-contain"
+                />
                 <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
                     A new way to make the payment easy, reliable and secure.
                 </p>
             </div>
+
             <div className="flex flex-[1.5] w-full flex-row justify-between flex-wrap md:mt-0 mt-10 ">
-                {footerLinks.map((footerLink) => (
-                    <div className="flex flex-col ss:my-0 my-4 min-w-[150px]" key={footerLink.key}>
+                {footerLinks.map((footerlink, index) => (
+                    <div className="flex flex-col ss:my-0 my-4 min-w-[150px]" key={index}>
                         <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
-                            {footerLink.title}
+                            {footerlink.title}
                         </h4>
                         <ul className="list-none mt-4">
-                            {footerLink.links.map((link,) => (
-                                <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer 
-                                ${index !== footerLink.links.length - 1 ? "mb-4" : "mb-0"}`}>
+                            {footerlink.links.map((link, index) => (
+                                <li
+                                    key={index}
+                                    className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer 
+                                    ${index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"}`}
+                                >
                                     {link.name}
                                 </li>
                             ))}
@@ -45,7 +55,7 @@ const Footer = () => (
                 ))}
             </div>
         </div>
-    </section >
+    </section>
 )
 
 
